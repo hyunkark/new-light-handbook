ea215d214a2326452580738e83ac99bcc65af4cb
hyunkark <hyunkark@gmail.com>
2023-03-13 22:51:32 +0900

    [Add] #main Config styled-components

---
+++ src/styles/GlobalStyle.ts
@@ -0,0 +1,16 @@
+import { createGlobalStyle } from "styled-components";
+import { normalize } from "styled-normalize";
+
+export const GlobalStyle = createGlobalStyle`
+  ${normalize}
+  html {
+    box-sizing: border-box;
+    font-size: 18px;
+    min-width: 320px;
+  }
+
+  a {
+    cursor: pointer;
+    text-decoration: none;
+  }
+`;
